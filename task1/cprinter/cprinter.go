caf0f258d7e26e1515c9e240a1e7022bbd1260b5
k1flar <den1s-davydov@mail.ru>
2024-03-28 17:30:45 +0300

    create xml-rpc server

---
+++ task1/cprinter/cprinter.go
@@ -0,0 +1,34 @@
+package cprinter
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Print(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %s", filePath)
+	}
+	defer file.Close()
+
+	cmd := exec.Command("cat", filePath)
+	cmd.Stdout = os.Stdout
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("error starting command: %v", err)
+	}
+
+	// _, err = io.Copy(cmd.Stdout, file)
+	// if err != nil {
+	// 	return fmt.Errorf("error writing to pipe: %v", err)
+	// }
+
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("error waiting for command: %v", err)
+	}
+
+	return nil
+}
--- task2/main.go
+++ task2/main.go
@@ -1,38 +1,106 @@
 package main
 
 import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
 	"io"
 	"io/fs"
 	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
+
+const (
+	FirstPort  = "8080"
+	SecondPort = "8081"
+)
+
+type Request struct {
+	XMLName  xml.Name `xml:"file"`
+	FilePath string   `xml:",chardata"`
+}
 
 func main() {
 	if len(os.Args) < 3 {
+		fmt.Println("no files")
 		os.Exit(1)
 	}
 
 	file1, file2 := os.Args[1], os.Args[2]
 	if file1 == "" || file2 == "" {
+		fmt.Println("no files")
 		os.Exit(1)
 	}
 
-	firstCmd := exec.Command("./bin/t1", file1)
-	secondCmd := exec.Command("./bin/t1", file2)
-	firstBytes := getBytes(firstCmd)
-	secondBytes := getBytes(secondCmd)
+	firstCmd := exec.Command("./bin/t1", FirstPort)
+	secondCmd := exec.Command("./bin/t1", SecondPort)
 
-	ioutil.WriteFile("res.txt", xorBytes(firstBytes, secondBytes), fs.FileMode(0777))
+	// starting xml-rpc servers
+	firstReader := startServer(firstCmd, FirstPort)
+	secondReader := startServer(secondCmd, SecondPort)
+
+	b1, err := getBytesFromServer(FirstPort, file1, firstReader)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	b2, err := getBytesFromServer(SecondPort, file2, secondReader)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ioutil.WriteFile("res.txt", xorBytes(b1, b2), fs.FileMode(0777))
+
+	firstCmd.Process.Kill()
+	secondCmd.Process.Kill()
 }
 
-func getBytes(command *exec.Cmd) []byte {
+func startServer(command *exec.Cmd, port string) io.Reader {
 	reader, writer := io.Pipe()
 	command.Stdout = writer
-	command.Start()
+	if err := command.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	waitServer(port)
+	return reader
+}
+
+func waitServer(port string) {
+	for !isPortOPen(port) {
+		time.Sleep(time.Second / 2)
+	}
+}
+
+func isPortOPen(port string) bool {
+	conn, err := net.Dial("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func getBytesFromServer(port string, file string, reader io.Reader) ([]byte, error) {
+	req := Request{FilePath: file}
+	body, _ := xml.Marshal(req)
+	_, err := http.Post(fmt.Sprintf("http://localhost:%s/rpc/print", port), "text/xml", bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: io.ReadAll не работает - т.к. процесс с прослушивающим портом не заканчивается
+	// и writer не закрывается
 	data := make([]byte, 1024)
-	n, _ := reader.Read(data)
-	return data[:n]
+	n, err := reader.Read(data)
+	if err != nil {
+		return nil, err
+	}
+	return data[:n], nil
 }
 
 func xorBytes(text, key []byte) []byte {
